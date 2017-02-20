01ce1cf19b3cc1e8a4073c94beccc79591dde1b0
anandrjoshi <anandrj@live.com>
2017-02-20 00:35:39 -0500

    MinimumGeneMutation Inputs

--- MinimumGeneticMutation/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ MinimumGeneticMutation/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -4,12 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace MinimumGeneticMutation
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			Console.WriteLine("Enter start gene sequence:");
+			string startSequence = Console.ReadLine();
+
+			Console.WriteLine("Enter end gene sequence:");
+			string endSequence = Console.ReadLine();
+
+			Console.WriteLine("{0}, {1}", startSequence, endSequence);
+
+			Console.ReadKey();
 		}
 	}
 }

440cfc5851a3f65e1375183cab458ccb090a17c1
anandrjoshi <anandrj@live.com>
2017-02-20 00:19:44 -0500

    Some existing projects

---
+++ FunWithNumbers/FunWithNumbers/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithNumbers
+{
+	class Program
+	{
+		public delegate void PrintSequence(int[] sequence);
+		public delegate int[] GenerateSequence(int number);
+
+		static void Main(string[] args)
+		{
+			
+			do
+			{
+				Console.Clear();
+				Console.WriteLine("Enter the number of inputs for the sequence:");
+				string input = Console.ReadLine();
+				int n;
+
+				Int32.TryParse(input, out n);
+
+				GenerateSequence fibonaccisequenceHandler = GenerateFibonacciSequence;
+				GenerateSequence triangleSequenceHandler = GenerateTriangleSequence;
+				int[] fibonacciSequence = fibonaccisequenceHandler(n);
+				int[] triangleSequence = triangleSequenceHandler(n);
+				
+				PrintSequence printFibonacciSequenceHandler = PrintFibonacciSequence;
+				printFibonacciSequenceHandler(fibonacciSequence);
+
+				PrintSequence printTriangleSequenceHandler = PrintTriangleSequence;
+				printTriangleSequenceHandler(triangleSequence);
+
+				Console.WriteLine("DO you want to continue(y/n).");
+
+
+			} while (string.Compare(Console.ReadLine(), "y") == 0);
+		}
+
+		private static void PrintTriangleSequence(int[] sequence)
+		{
+			Console.WriteLine("Triangle sequence for first {0} numbers is:", sequence.Count());
+
+			foreach (int i in sequence)
+			{
+				Console.WriteLine(i);
+			}
+		}
+
+		private static int[] GenerateTriangleSequence(int number)
+		{
+			int[] triangleSequence = new int[number];
+
+			if (number == 1)
+			{
+				triangleSequence[0] = 1;
+				return triangleSequence;
+			}
+			else if (number == 2)
+			{
+				triangleSequence[0] = 1;
+				triangleSequence[1] = 3;
+				return triangleSequence;
+			}
+			else if (number > 2)
+			{
+				for (int i = 1; i <= number; i++)
+				{
+					triangleSequence[i-1] = i*(i+1)/2;
+				}
+				return triangleSequence;
+			}
+			else
+			{
+				Console.WriteLine("Incorrect input.");
+				return null;
+			}
+		}
+
+		private static void PrintFibonacciSequence(int[] sequence)
+		{
+			Console.WriteLine("Fibonacci sequence for first {0} numbers is:", sequence.Count());
+
+			foreach (int i in sequence)
+			{
+				Console.WriteLine(i);
+			}
+		}
+
+		private static int[] GenerateFibonacciSequence(int n)
+		{
+			int[] fibonacciSequence = new int[n];
+			
+			if(n == 1)
+			{
+				fibonacciSequence[0] = 1;
+				return fibonacciSequence;
+			}
+			else if(n == 2)
+			{
+				fibonacciSequence[0] = 1;
+				fibonacciSequence[1] = 1;
+				return fibonacciSequence;
+			}
+			else if(n > 2)
+			{
+				fibonacciSequence[0] = 1;
+				fibonacciSequence[1] = 1;
+				for(int i = 2; i < n; i++)
+				{
+					fibonacciSequence[i] = fibonacciSequence[i-1] + fibonacciSequence[i-2]; 
+				}
+				return fibonacciSequence;
+			}
+			else
+			{
+				Console.WriteLine("Incorrect input.");
+				return null;
+			}
+		}
+	}
+}
---
+++ TryDelegates/TryDelegates/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryDelegates
+{
+	class Program
+	{
+		delegate string DateTimeToString(DateTime time);
+
+		static void Main(string[] args)
+		{
+			DateTimeToString dateFirst = new DateTimeToString(DateFirst);
+
+			DateTimeToString yearFirst = new DateTimeToString(YearFirst);
+
+			Console.WriteLine("Time:" + DateTime.Now);
+			Console.WriteLine("DateFirst Time:" + dateFirst(DateTime.Now));
+			Console.WriteLine("YearFirst Time:" + yearFirst(DateTime.Now));
+			Console.ReadKey();
+		}
+
+		public static string DateFirst(DateTime time)
+		{
+			StringBuilder timeString = new StringBuilder();
+
+			timeString.Append(time.Day);
+			timeString.Append("/");
+			timeString.Append(time.Month);
+			timeString.Append("/");
+			timeString.Append(time.Year);
+			timeString.Append(" ");
+
+			timeString.Append(time.Hour > 11 ? time.Hour -12 : time.Hour);
+			timeString.Append(":");
+			timeString.Append(time.Minute);
+			timeString.Append(":");
+			timeString.Append(time.Second);
+			timeString.Append(":");
+			timeString.Append(time.Hour < 11 ? "AM" : "PM");
+
+			return timeString.ToString();
+		}
+
+		public static string YearFirst(DateTime time)
+		{
+			StringBuilder timeString = new StringBuilder();
+
+			timeString.Append(time.Year);
+			timeString.Append("/");
+			timeString.Append(time.Month);
+			timeString.Append("/");
+			timeString.Append(time.Day);
+			timeString.Append(" ");
+
+			timeString.Append(time.Hour > 11 ? time.Hour - 12 : time.Hour);
+			timeString.Append(":");
+			timeString.Append(time.Minute);
+			timeString.Append(":");
+			timeString.Append(time.Second);
+			timeString.Append(":");
+			timeString.Append(time.Hour < 11 ? "AM" : "PM");
+
+			return timeString.ToString();
+		}
+	}
+}

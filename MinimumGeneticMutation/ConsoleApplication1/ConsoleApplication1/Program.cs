493aa43b0587b5d5d301e00a067722e637829dbc
anandrjoshi <anandrj@live.com>
2017-02-20 01:11:32 -0500

    MinimumGeneSequence without bank.

--- MinimumGeneticMutation/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ MinimumGeneticMutation/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -18,7 +18,73 @@ namespace MinimumGeneticMutation
 
 			Console.WriteLine("{0}, {1}", startSequence, endSequence);
 
+			int numberOfMutations = CalcutateMutations(startSequence, endSequence);
 			Console.ReadKey();
+		}
+
+		private static int CalcutateMutations(string startSequence, string endSequence)
+		{
+			StringBuilder currentSequence = new StringBuilder(startSequence);
+			int mutations = 0;
+			
+			if(!ValidateInputs(startSequence, endSequence))
+			{
+				Console.WriteLine("Invalid inputs.");
+				return -1;
+			}
+			for (int i = 0; i < startSequence.Length; i++)
+			{
+				if(startSequence[i] != endSequence[i])
+				{
+					currentSequence.Remove(i, 1);
+					currentSequence.Insert(i, endSequence[i]);
+
+					mutations++;
+
+					Console.WriteLine("CurrentSequence = {0}", currentSequence);
+					Console.WriteLine("Mutation Count = {0}", mutations);
+				}
+				else
+				{
+					Console.WriteLine("No mutation at position {0}", i);
+				}
+				
+			}
+			
+			return mutations;
+		}
+
+		private static bool ValidateInputs(string startSequence, string endSequence)
+		{
+			if(startSequence.Length != endSequence.Length)
+			{
+				return false;
+			}
+			else if(!ValidateCorrectGenes(startSequence))
+			{
+				return false;
+			}
+			else if (!ValidateCorrectGenes(endSequence))
+			{
+				return false;
+			}
+			else
+			{
+				return true;
+			}
+		}
+
+		private static bool ValidateCorrectGenes(string sequence)
+		{
+			foreach(char c in sequence)
+			{
+				if(c == 'A' || c == 'C' || c == 'G' || c == 'T')
+				{
+					continue;
+				}
+				return false;
+			}
+			return true;
 		}
 	}
 }

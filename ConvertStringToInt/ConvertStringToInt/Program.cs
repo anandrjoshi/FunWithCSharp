8e5d4193e0883bcf3357948bcc2f56cfeab5bca8
anandrjoshi <anandrj@live.com>
2017-02-26 00:51:43 -0500

    FIzzBuzz and ConvertStringToInt

---
+++ ConvertStringToInt/ConvertStringToInt/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertStringToInt
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter number as string:");
+			string text = Console.ReadLine();
+
+			int integer = ConvertStringToInt(text);
+
+			Console.WriteLine("Int value = {0}", integer);
+
+			Console.ReadKey();
+		}
+
+		private static int ConvertStringToInt(string text)
+		{
+			char[] input = text.Trim().ToCharArray();
+			Array.Reverse(input);
+
+			int returnValue = 0;
+			int multiplier = 1;
+
+			foreach (char c in input)
+			{
+				int unicode = c;
+				if(unicode > 47 && unicode < 58)
+				{
+					returnValue += (unicode - 48) * multiplier;
+					multiplier *= 10;
+				}
+				else if(unicode == 45)
+				{
+					returnValue *= -1;
+				}
+				else if (unicode == 43)
+				{
+					returnValue *= 1;
+				}
+				else
+				{
+					throw new ArgumentNullException("Invalid input.");
+				}
+			}
+
+			return returnValue;
+		}
+
+		
+	}
+}
---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter value for n: ");
+			int n = Convert.ToInt32(Console.ReadLine());
+
+			if(n<0)
+			{
+				Console.WriteLine("Validation failed!");
+			}
+			else
+			{ 
+				IList<string> fizzBuzz = GenerateFizzBuzzSequence(n);
+
+				foreach(string str in fizzBuzz)
+				{
+					Console.WriteLine(str);
+				}
+			}
+
+			Console.ReadKey();
+		}
+
+		private static IList<string> GenerateFizzBuzzSequence(int n)
+		{
+			IList<string> fizzBuzz = new List<string>();
+			
+			for (int i = 0; i < n; i++)
+			{
+				if((i+1)%3 == 0 && (i+1)%5 == 0)
+				{
+					fizzBuzz.Add("FizzBuzz");
+				}
+				else if((i+1)%3 == 0)
+				{
+					fizzBuzz.Add("Fizz");
+				}
+				else if((i+1)%5 == 0)
+				{
+					fizzBuzz.Add("Buzz");
+				}
+				else
+				{
+					fizzBuzz.Add(Convert.ToString(i+1));
+				}
+			}
+
+			return fizzBuzz;
+		}
+	}
+}

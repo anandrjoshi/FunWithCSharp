1c4d6f8503c8843e41a46d5d0892b3ef5703d863
Anand Joshi <ajoshi@ezesoft.com>
2017-03-01 17:31:46 -0500

    Added Bubble Sort.

---
+++ SortingAlgorithms/SortingAlgorithms/Classes/BubbleSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms.Classes
+{
+	public static class BubbleSort
+	{
+		public static void SortIntegers(int[] arrayOfInts)
+		{
+			int length = arrayOfInts.Length;
+
+			while (length > 0)
+			{
+				for (int i = 1; i < arrayOfInts.Length; i ++)
+				{
+					if (arrayOfInts[i - 1] > arrayOfInts[i])
+					{
+						int temp = arrayOfInts[i - 1];
+						arrayOfInts[i - 1] = arrayOfInts[i];
+						arrayOfInts[i] = temp;
+					}
+				}
+				length--;
+			}
+		}
+	}
+}
---
+++ SortingAlgorithms/SortingAlgorithms/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SortingAlgorithms.Classes;
+
+namespace SortingAlgorithms
+{
+	class Program
+	{
+		public delegate void SortingAlgorithm(int[] arrayOfInts);
+
+		static void Main(string[] args)
+		{
+			int[] arrayOfInts = {22,11,1,23,45,12,34,67,34,89,77,16,01,38,90,56,65,33,17,19,2,9,87,54,93,96,52,77,6,89,32,54,89,44,46,41,76,72,70,86,84,31,07,61,67,63};
+
+			int[] tempArray = arrayOfInts.Clone() as int[];
+
+			Stopwatch watch = DoSortIntegers(BubbleSort.SortIntegers, tempArray);
+
+			Console.WriteLine("Un Sorted Array = {0} ", BuildOutputString(arrayOfInts));
+			Console.WriteLine("Sorted Array = {0} ", BuildOutputString(tempArray));
+			Console.WriteLine("Time taken = {0} ", watch.ElapsedTicks);
+
+			Console.ReadKey();
+		}
+
+		private static string BuildOutputString(int[] array)
+		{
+			StringBuilder stringBuilder = new StringBuilder();
+			stringBuilder.Append("[ ");
+
+			foreach (int i in array)
+			{
+				stringBuilder.Append(i);
+				stringBuilder.Append(",");
+			}
+
+			stringBuilder.Append(" ]");
+
+			return stringBuilder.ToString();
+		}
+
+		private static Stopwatch DoSortIntegers(SortingAlgorithm algorithm, int[] arrayInts)
+		{
+			Stopwatch watch = new Stopwatch();
+			watch.Start();
+			algorithm(arrayInts);
+			watch.Stop();
+
+			return watch;
+		}
+	}
+}

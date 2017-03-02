8c8e426181ba894e477841f2892613cd0649f0a7
anandrjoshi <anandrj@live.com>
2017-03-01 22:53:24 -0500

    Added quicksort implementation

--- SortingAlgorithms/SortingAlgorithms/Classes/QuickSort.cs
+++ SortingAlgorithms/SortingAlgorithms/Classes/QuickSort.cs
@@ -11,7 +11,40 @@ namespace SortingAlgorithms.Classes
 	{
 		public static void SortIntegers(int[] arrayOfInts)
 		{
-			
+			DoQuickSort(arrayOfInts, 0, arrayOfInts.Length-1);
+		}
+
+		private static void DoQuickSort(int[] arrayOfInts, int low, int high)
+		{
+			if(low < high)
+			{
+				int pivot = Partition(arrayOfInts, low, high);
+				DoQuickSort(arrayOfInts, low, pivot-1);
+				DoQuickSort(arrayOfInts, pivot+1, high);
+			}
+		}
+
+		private static int Partition(int[] arrayOfInts, int low, int high)
+		{
+			int pivot = arrayOfInts[low];
+			int i = low;
+
+			for(int j = low+1; j < high+1; j++)
+			{
+				if(arrayOfInts[j] <= pivot)
+				{
+					i++;
+					int temp = arrayOfInts[i];
+					arrayOfInts[i] = arrayOfInts[j];
+					arrayOfInts[j] = temp;
+				}
+			}
+
+			int temp1 = arrayOfInts[i];
+			arrayOfInts[i] = arrayOfInts[low];
+			arrayOfInts[low] = temp1;
+
+			return i ;
 		}
 	}
 }
--- SortingAlgorithms/SortingAlgorithms/Program.cs
+++ SortingAlgorithms/SortingAlgorithms/Program.cs
@@ -14,13 +14,17 @@ namespace SortingAlgorithms
 
 		static void Main(string[] args)
 		{
-			int[] arrayOfInts = {22,11,1,23,45,12,34,67,34,89,77,16,01,38,90,56,65,33,17,19,2,9,87,54,93,96,52,77,6,89,32,54,89,44,46,41,76,72,70,86,84,31,07,61,67,63};
+			int[] arrayOfInts = {36,35,40,39,30,5,14,13,16,17,18,20,83,84,24,28,27,26,10,90,89,78,67,48,68,97,
+									56,45,43,31,22,94,34,43,56,37,98,58,98,87,84,81,71,51,50,42,49,47,75,91,
+									64,63,08,84,86,88,31,51,76,87,95,92,66,55,44,33,22,11,88,99,80,60,59,82,
+									4,6,7,2,31,51,62,21,32,25,21,29,99,94,76,54,84,03,45,69,87,22,85,53,73,
+									11,1,23,45,12,34,67,34,89,77,16,01,38,90,56,65,33,17,19,2,9,87,88,57,74,
+									54,93,96,52,77,6,89,32,54,89,44,46,41,76,72,70,86,84,31,07,61,67,63};
 
 			int[] bubbleSortInputArray = arrayOfInts.Clone() as int[];
 
 			Stopwatch watchBubbleSort = DoSortIntegers(BubbleSort.SortIntegers, bubbleSortInputArray);
 
-			Console.WriteLine("Un Sorted Array = {0} ", BuildOutputString(arrayOfInts));
 			Console.WriteLine("Sorted Array with Bubble Sort = {0} ", BuildOutputString(bubbleSortInputArray));
 			Console.WriteLine("Time taken for Bubble Sort = {0} ", watchBubbleSort.ElapsedTicks);
 
@@ -28,7 +32,6 @@ namespace SortingAlgorithms
 
 			Stopwatch watchSelectionSort = DoSortIntegers(SelectionSort.SortIntegers, selectionSortInputArray);
 
-			Console.WriteLine("Un Sorted Array = {0} ", BuildOutputString(arrayOfInts));
 			Console.WriteLine("Sorted Array with Selection Sort = {0} ", BuildOutputString(selectionSortInputArray));
 			Console.WriteLine("Time taken for Selection Sort = {0} ", watchSelectionSort.ElapsedTicks);
 
@@ -36,9 +39,15 @@ namespace SortingAlgorithms
 
 			Stopwatch watchInsertionSort = DoSortIntegers(InsertionSort.SortIntegers, insertionSortInputArray);
 
-			Console.WriteLine("Un Sorted Array = {0} ", BuildOutputString(arrayOfInts));
 			Console.WriteLine("Sorted Array with Insertion Sort = {0} ", BuildOutputString(insertionSortInputArray));
 			Console.WriteLine("Time taken for Insertion Sort = {0} ", watchInsertionSort.ElapsedTicks);
+
+			int[] quickSortInputArray = arrayOfInts.Clone() as int[];
+
+			Stopwatch watchQuickSort = DoSortIntegers(QuickSort.SortIntegers, quickSortInputArray);
+
+			Console.WriteLine("Sorted Array with Quick Sort = {0} ", BuildOutputString(quickSortInputArray));
+			Console.WriteLine("Time taken for Quick Sort = {0} ", watchQuickSort.ElapsedTicks);
 
 			Console.ReadKey();
 		}

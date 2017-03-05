53ec2a93906f11ac9098f5bec8420850786b1435
anandrjoshi <anandrj@live.com>
2017-03-04 23:38:54 -0500

    Palindrome for integres and strings.

---
+++ Palindrome/Palindrome/Classes/Palindrome.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome.Classes
+{
+	public static class Palindrome
+	{
+		public static bool CheckPalindromeString(string inputString)
+		{
+			char[] inputASCharArray = inputString.ToCharArray();
+
+			for (int i = 0; i < inputASCharArray.Length / 2; i++)
+			{
+				if(inputASCharArray[i] != inputASCharArray[inputASCharArray.Length - 1 - i])
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+		public static bool CheckPalindromeInt(int inputInt)
+		{
+			string inputIntAsString = Convert.ToString(inputInt);
+			int length = inputIntAsString.Length;
+
+			int[] arrayOfInts = new int[length];
+
+			for (int i = length; i > 0; i-- )
+			{
+				arrayOfInts[i-1] = inputInt / Convert.ToInt32(Math.Pow(10, (i - 1)));
+				inputInt -= arrayOfInts[i - 1] * Convert.ToInt32(Math.Pow(10, (i - 1)));
+			}
+
+			for (int i = 0; i < arrayOfInts.Length/2; i++ )
+			{
+				if (arrayOfInts[i] != arrayOfInts[arrayOfInts.Length - 1 - i])
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+}
---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter input string:");
+			string inputString = Console.ReadLine();
+
+			bool isStringPalindrome = Palindrome.Classes.Palindrome.CheckPalindromeString(inputString);
+			string outputString = isStringPalindrome ? "is Palindrome" : "is not Plaindrome";
+
+			Console.WriteLine("Input string {0} : {1}", inputString, outputString);
+
+			Console.WriteLine("Enter input int:");
+			int inputInt = Convert.ToInt32(Console.ReadLine());
+
+			bool isIntPalindrome = Palindrome.Classes.Palindrome.CheckPalindromeInt(inputInt);
+			outputString = isIntPalindrome ? "is Palindrome" : "is not Plaindrome";
+
+			Console.WriteLine("Input int {0} : {1}", inputInt, outputString);
+			Console.ReadKey();
+		}
+	}
+}
